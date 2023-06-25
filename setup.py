ee2058597338c008a95114d1f166adc88017040e
Yuki Tatsunami <okojo.yt@gmail.com>
2023-06-26 04:25:00 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ setup(
     name='taming-transformers',
     version='0.0.1',
     description='Taming Transformers for High-Resolution Image Synthesis',
-    packages=find_packages(),
+    packages=find_namespace_packages(include=["taming", "taming.*"]),
     install_requires=[
         'torch',
         'numpy',
         'tqdm',
+        'pytorch-lightning',
+        'einops'
     ],
 )
